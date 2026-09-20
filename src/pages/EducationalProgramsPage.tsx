ba0fd8a386bf8370cfc9b01ce09bdd2184211e2e
restary259 <ranimdwahde3@gmail.com>
2026-09-20 17:43:44 +0200

    refactor(cta): remove unnecessary contact CTA

--- src/pages/EducationalProgramsPage.tsx
+++ src/pages/EducationalProgramsPage.tsx
@@ -8,7 +8,6 @@ import MajorCard from '@/components/educational/MajorCard';
 import HeroSection from '@/components/educational/HeroSection';
 import SearchAndFilter from '@/components/educational/SearchAndFilter';
 import NoResults from '@/components/educational/NoResults';
-import DarbContactCta from '@/components/common/DarbContactCta';
 import SEOHead from '@/components/common/SEOHead';
 import { useDirection } from '@/hooks/useDirection';
 import { useTranslation } from 'react-i18next';
@@ -135,7 +134,7 @@ const EducationalProgramsPage = () => {
           </div>
         </div>
       </section>
-      <DarbContactCta variant="compact" />
+
       <MajorModal isOpen={isModalOpen} onClose={handleCloseModal} major={selectedMajor} />
       <Footer />
     </div>
