d7de2e3107f263efde099c96bfa1a57b57563ae8
restary259 <ranimdwahde3@gmail.com>
2026-09-20 15:09:06 +0200

    Apply shared DARB hero treatment

--- src/pages/EducationalDestinationsPage.tsx
+++ src/pages/EducationalDestinationsPage.tsx
@@ -11,6 +11,8 @@ import { universities, languageSchools, services } from '@/data/educationalDesti
 import SEOHead from '@/components/common/SEOHead';
 import DarbPageHero from '@/components/common/DarbPageHero';
 import { DARB_PUBLIC_HERO_IMAGES } from '@/config/publicHeroImages';
+import DarbPageHero from '@/components/common/DarbPageHero';
+import { DARB_PUBLIC_HERO_IMAGES } from '@/config/publicHeroImages';
 
 import { useTranslation } from 'react-i18next';
 import { useDirection } from '@/hooks/useDirection';
