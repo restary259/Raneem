cce6395330523ccaecefe2686a6279377092bacc
restary259 <ranimdwahde3@gmail.com>
2026-09-20 17:44:30 +0200

    refactor(cta): remove low-intent contact strip

--- src/components/quiz/AIQuizChat.tsx
+++ src/components/quiz/AIQuizChat.tsx
@@ -15,7 +15,6 @@ import ChatOfflineBanner from '@/components/chat/ChatOfflineBanner';
 import ChatQuickQuestions from '@/components/chat/ChatQuickQuestions';
 import { useTranslation } from 'react-i18next';
 import { useDirection } from '@/hooks/useDirection';
-import DarbContactCta from '@/components/common/DarbContactCta';
 
 const AIQuizChat = () => {
   const { t } = useTranslation();
@@ -97,7 +96,6 @@ const AIQuizChat = () => {
         </div>
       </main>
 
-      <DarbContactCta variant="compact" />
       <Footer />
     </div>
   );
